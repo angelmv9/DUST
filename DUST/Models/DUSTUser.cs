46cc59802aada7a1c90584ed0bedad3a125e8410
angelmv9 <angelmorfa92@gmail.com>
2022-08-16 09:12:35 -0400

    Add project files.

---
+++ DUST/Models/DUSTUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DUST.Models
+{
+    public class DUSTUser : IdentityUser
+    {
+        [Required]
+        [StringLength(25, ErrorMessage = "{0} must be at least {2} and no more than {1} characters long", MinimumLength = 3)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage = "{0} must be at least {2} and no more than {1} characters long", MinimumLength = 1)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+    }
+}
