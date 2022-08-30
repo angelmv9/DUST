e55c8486690af63dfe43d5f1bb7cde6547f1f615
angelmv9 <angelmorfa92@gmail.com>
2022-08-30 05:09:00 -0400

    Updated DUSTUser model by adding Avatar properties, companyId & collection of Projects

--- DUST/Models/DUSTUser.cs
+++ DUST/Models/DUSTUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -10,6 +11,9 @@ namespace DUST.Models
 {
     public class DUSTUser : IdentityUser
     {
+        // Foreign Key
+        public int? CompanyId { get; set; }
+
         [Required]
         [StringLength(25, ErrorMessage = "{0} must be at least {2} and no more than {1} characters long", MinimumLength = 3)]
         [Display(Name = "First Name")]
@@ -23,6 +27,27 @@ namespace DUST.Models
         [NotMapped]
         [Display(Name = "Full Name")]
         public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        [Display(Name ="Avatar Image")]
+        public IFormFile AvatarFormFile { get; set; }
+
+        public byte[] AvatarByteData { get; set; }
+
+        [Display(Name = "File Name")]
+        public string AvatarFileName { get; set; }
+
+        [Display(Name = "File Extension")]
+        public string AvatarFileExtension { get; set; }
+
+        /* Navigation Properties */
+
+        // Parents of DUSTUser
+        public virtual Company Company { get; set; }
+
+        // Children of DUSTUser
+        public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>();
 
     }
 }
