34a9d08708f8215637a7a8ee8133ca7ddafcae91
angelmv9 <angelmorfa92@gmail.com>
2022-08-30 04:43:23 -0400

    Added Company model

---
+++ DUST/Models/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DUST.Models
+{
+    public class Company
+    {
+        // Primary key
+        public int Id { get; set; }
+
+        [DisplayName("Company Name")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        /* Navigation properties */
+
+        // Children of Company
+        public virtual ICollection<DUSTUser> Members { get; set; } = new HashSet<DUSTUser>();
+        public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>();
+    }
+}
