61d5bc6758310375a7e9c507544d77c4f8d33d17
angelmv9 <angelmorfa92@gmail.com>
2022-08-31 07:15:52 -0400

    Added DbSets to ApplicationDbContext, added migration & updated database

--- DUST/Data/Migrations/20220831111051_Data Models.Designer.cs
+++ DUST/Data/Migrations/20220831111051_Data Models.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using DUST.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace DUST.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220831111051_Data Models")]
+    partial class DataModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
