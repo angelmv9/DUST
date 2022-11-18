0fb21c27255cd32490b3c0015085de793511a994
angelmv9 <angelmorfa92@gmail.com>
2022-11-18 05:49:19 -0500

    Added enums and an empty DataUtilities class

---
+++ DUST/Models/Enums/TicketType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DUST.Models.Enums
+{
+    public enum TicketType
+    {
+        NewDevelopment,
+        WorkTask,
+        Defect,
+        ChangeRequest,
+        Enhancement,
+        GeneralTask
+    }
+}
