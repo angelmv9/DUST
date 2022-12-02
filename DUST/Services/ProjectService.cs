7c90192320a727e71576af5970011ac5bfb0113f
Angel <50785538+angelmv9@users.noreply.github.com>
2022-12-02 06:58:07 -0500

    Updated GetArchivedProjectsByCompanyAsync to actually return archived  projects

--- DUST/Services/ProjectService.cs
+++ DUST/Services/ProjectService.cs
@@ -168,9 +168,29 @@ namespace DUST.Services
 
         public async Task<List<Project>> GetArchivedProjectsByCompanyAsync(int companyId)
         {
-            List<Project> projects = await GetAllActiveProjectsByCompanyAsync(companyId);
-
-            return projects.Where(p => p.Archived == true).ToList();
+            List<Project> projects = await _context.Projects.Where(p => p.CompanyId == companyId && p.Archived == true)
+                                            .Include(p => p.Members)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.Comments)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.Attachments)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.History)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.DeveloperUser)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.OwnerUser)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.Notifications)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketStatus)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketPriority)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketType)
+                                            .Include(p => p.ProjectPriority)
+                                            .ToListAsync();
+            return projects;
         }
 
         public Task<List<DUSTUser>> GetDevelopersOnProjectAsync(int projectId)
