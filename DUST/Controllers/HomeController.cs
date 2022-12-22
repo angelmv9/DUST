bc3c07cd5ee2348ba23f014cd5c88483e80fe642
Angel <50785538+angelmv9@users.noreply.github.com>
2022-12-22 09:22:01 -0500

    Added dashboard action and view

--- DUST/Controllers/HomeController.cs
+++ DUST/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using DUST.Models;
+﻿using DUST.Extensions;
+using DUST.Models;
+using DUST.Models.ViewModels;
+using DUST.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,16 +14,31 @@ namespace DUST.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICompanyInfoService _companyService;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICompanyInfoService companyService)
         {
             _logger = logger;
+            _companyService = companyService;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            DashboardViewModel model = new();
+            model.Company = await _companyService.GetCompanyInfoByIdAsync(companyId);
+            model.Projects = (await _companyService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false).ToList();
+            model.Tickets = model.Projects.SelectMany(p => p.Tickets).Where(t => t.Archived == false).ToList();
+            model.Members = model.Company.Members.ToList();
+
+            return View(model);
         }
 
         public IActionResult Privacy()
