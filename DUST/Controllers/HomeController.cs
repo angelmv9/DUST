21438cc053f1f2f043b719fd0349be9fd9ebe1be
Angel <50785538+angelmv9@users.noreply.github.com>
2023-02-02 15:36:00 -0500

    Carved in most Identity pages, only confirmEmailChange left

--- DUST/Controllers/HomeController.cs
+++ DUST/Controllers/HomeController.cs
@@ -150,10 +150,10 @@ namespace DUST.Controllers
 			return Json(plotlyData);
 		}
 
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
